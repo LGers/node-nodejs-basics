d1235533a0af8cc5f3a3b4889cae565d353fe7b4
LGers <leonid.mychko2k21@gmail.com>
2023-06-17 11:14:22 +0300

    feat: implement copy.js

--- src/fs/constants.js
+++ src/fs/constants.js
@@ -2,3 +2,7 @@ export const MESSAGES = {
   FS_ERROR: 'FS operation failed',
   SUCCESS: 'File created',
 };
+
+export const PATH_TO_FILES = './src/fs/files';
+export const PATH_TO_COPY = './src/fs/files_copy';
+
