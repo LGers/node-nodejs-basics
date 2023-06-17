b4af23d4f5d0161ca02e56b939e30d76c15ae605
LGers <leonid.mychko2k21@gmail.com>
2023-06-17 13:20:27 +0300

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import { MESSAGES, PATH_TO_FILES } from './constants.js';
+import fs from 'fs';
+
+const { FS_ERROR } = MESSAGES;
+
+const pathToReadFilename = `${PATH_TO_FILES}/fileToRead.txt`;
+const uniCode = 'utf8';
+
 const read = async () => {
-    // Write your code here 
+  return new Promise((res, rej) => {
+    if (fs.existsSync(pathToReadFilename)) {
+      fs.readFile(pathToReadFilename, uniCode, (err, data) => {
+        if (err) {
+          rej(err);
+        }
+
+        console.log(data);
+        res(data);
+      });
+    } else {
+      throw new Error(FS_ERROR);
+    }
+  });
 };
 
 await read();
