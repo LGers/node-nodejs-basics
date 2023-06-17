d1b302ca53a00766c744713351e9baf9686147d6
LGers <leonid.mychko2k21@gmail.com>
2023-06-17 11:56:42 +0300

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,26 @@
+import { MESSAGES, PATH_TO_FILES } from './constants.js';
+import fs from 'fs';
+
+const wrongFilename = 'fileToRemove.txt';
+
+const { FS_ERROR, SUCCESS } = MESSAGES;
+
+const pathToDeleteFilename = `${PATH_TO_FILES}/${wrongFilename}`;
+
 const remove = async () => {
-    // Write your code here 
+  return new Promise((res, rej) => {
+    if (fs.existsSync(pathToDeleteFilename)) {
+      fs.unlink(pathToDeleteFilename, (err) => {
+        if (err) {
+          rej(err);
+        }
+
+        res(SUCCESS);
+      });
+    } else {
+      throw new Error(FS_ERROR);
+    }
+  });
 };
 
 await remove();
