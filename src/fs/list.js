a6c5f054c7cb1cddf0c0ce048aa9cfb114a07e94
LGers <leonid.mychko2k21@gmail.com>
2023-06-17 13:14:06 +0300

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { MESSAGES, PATH_TO_FILES } from './constants.js';
+import fs from 'fs';
+
+const { FS_ERROR } = MESSAGES;
+
 const list = async () => {
-    // Write your code here 
+  return new Promise((res, rej) => {
+    if (fs.existsSync(PATH_TO_FILES)) {
+      fs.readdir(PATH_TO_FILES, (err, files) => {
+        if (err) {
+          rej(err);
+        }
+
+        console.log(files);
+        res(files);
+      });
+    } else {
+      throw new Error(FS_ERROR);
+    }
+  });
 };
 
 await list();
