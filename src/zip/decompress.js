a11f7ad4dfceac7eda88f3bc0d6ffe6bf8fc59e8
LGers <leonid.mychko2k21@gmail.com>
2023-06-18 15:13:44 +0300

    feat: implemet zip:decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream';
+
+import {
+  createReadStream,
+  createWriteStream,
+} from 'fs';
+
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFile = `${__dirname}/files/fileToCompress.txt`;
+const pathToFileZip = `${__dirname}/files/archive.gz`;
+
 const decompress = async () => {
-    // Write your code here 
+  const unzip = createUnzip();
+  const source = createReadStream(pathToFileZip);
+  const destination = createWriteStream(pathToFile);
+
+  pipeline(source, unzip, destination, (err) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await decompress();
