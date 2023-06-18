2b1b1ba546925f7b85df25efe4826847ea841d51
LGers <leonid.mychko2k21@gmail.com>
2023-06-18 10:22:41 +0300

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import { createHash } from 'crypto';
+import fs from 'fs';
+
+const uniCode = 'utf8';
+const pathToFile = './src/hash/files/fileToCalculateHashFor.txt';
+
+const readFile = (pathToFile) => {
+  return new Promise((res, rej) => {
+    try {
+      fs.readFile(pathToFile, uniCode, (err, data) => {
+        if (err) {
+          throw new Error(err);
+        }
+        res(data);
+      });
+    } catch (err) {
+      rej(new Error(err));
+    }
+  });
+};
+
 const calculateHash = async () => {
-    // Write your code here 
+  const data = await readFile(pathToFile);
+
+  const hash = createHash('sha256').update(data).digest('hex');
+
+  console.log(hash);
+  return hash;
 };
 
 await calculateHash();
