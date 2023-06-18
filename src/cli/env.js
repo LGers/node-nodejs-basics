d7485a071287b4b5ed218997857142ffa3cc7324
LGers <leonid.mychko2k21@gmail.com>
2023-06-18 09:06:30 +0300

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssValues = Object.entries(process.env).filter((item) => item[0].slice(0, 4) === 'RSS_');
+  const parsedEnv = rssValues.map((item) => item.join('=')).join('; ')
+  console.log(parsedEnv);
+
+  return parsedEnv
 };
 
 parseEnv();
