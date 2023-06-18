3758625a10a06df13abc4187d6d6f3ab0137228b
LGers <leonid.mychko2k21@gmail.com>
2023-06-18 11:06:47 +0300

    feat: implemet streams: read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { createReadStream } from 'fs';
+import { MESSAGES } from '../fs/constants.js';
+
+const uniCode = 'utf8';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFile = `${__dirname}/files/fileToRead.txt`;
+
 const read = async () => {
-    // Write your code here 
+  const readable = createReadStream(pathToFile, { encoding: uniCode });
+
+  readable.on('error', (err) => {
+    if (err.code === 'ENOENT') {
+      throw new Error(MESSAGES.FS_ERROR);
+    }
+  });
+
+  readable.pipe(process.stdout);
 };
 
 await read();
